aefd37a16ed8bdfb04ed101684459390692cb885
VioletXF <zudevxf@gmail.com>
2024-05-14 13:40:17 +0900

    Update libraries

--- include/SDL2/SDL_joystick.h
+++ include/SDL2/SDL_joystick.h
@@ -41,12 +41,12 @@
 #ifndef SDL_joystick_h_
 #define SDL_joystick_h_
 
-#include "SDL_stdinc.h"
-#include "SDL_error.h"
-#include "SDL_guid.h"
-#include "SDL_mutex.h"
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_guid.h>
+#include <SDL2/SDL_mutex.h>
 
-#include "begin_code.h"
+#include <SDL2/begin_code.h>
 /* Set up for C function definitions, even when using C++ */
 #ifdef __cplusplus
 extern "C" {
@@ -1062,7 +1062,7 @@ extern DECLSPEC SDL_JoystickPowerLevel SDLCALL SDL_JoystickCurrentPowerLevel(SDL
 #ifdef __cplusplus
 }
 #endif
-#include "close_code.h"
+#include <SDL2/close_code.h>
 
 #endif /* SDL_joystick_h_ */
 
