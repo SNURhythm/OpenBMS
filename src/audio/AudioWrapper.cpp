a8ad0ce80b2917cb38895e3f5ccb4361cdfdd6d3
VioletXF <zudevxf@gmail.com>
2024-05-22 12:37:39 +0900

    Add miniaudio

---
+++ src/audio/AudioWrapper.cpp
@@ -0,0 +1,51 @@
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "AudioWrapper.h"
+#include <stdexcept>
+#include <SDL2/SDL.h>
+AudioWrapper::AudioWrapper() {
+  auto result = ma_engine_init(nullptr, &engine);
+  if (result != MA_SUCCESS) {
+    throw std::runtime_error("Failed to initialize audio engine.");
+  }
+}
+
+AudioWrapper::~AudioWrapper() {
+  ma_engine_uninit(&engine);
+  unloadSounds();
+}
+
+void AudioWrapper::loadSound(const char *path) {
+  SDL_Log("Loading sound: %s", path);
+  auto result = ma_sound_init_from_file(&engine, path, MA_SOUND_FLAG_DECODE,
+                                        nullptr, nullptr, &sounds[path]);
+  if (result != MA_SUCCESS) {
+    throw std::runtime_error("Failed to load sound.");
+  }
+}
+
+void AudioWrapper::preloadSounds(const std::vector<std::string> &paths) {
+  for (const auto &path : paths) {
+    loadSound(path.c_str());
+  }
+}
+
+void AudioWrapper::playSound(const char *path) {
+  if (sounds.find(path) == sounds.end()) {
+    loadSound(path);
+  }
+  ma_engine_play_sound(&engine, path, nullptr);
+}
+
+void AudioWrapper::unloadSound(const char *path) {
+  ma_sound sound = sounds[path];
+  ma_sound_uninit(&sound);
+  sounds.erase(path);
+}
+
+void AudioWrapper::unloadSounds() {
+  for (auto &sound : sounds) {
+    ma_sound_uninit(&sound.second);
+  }
+  sounds.clear();
+}
