71582af1498dab4b1be7fb9afec20ca22b7978db
VioletXF <zudevxf@gmail.com>
2024-07-04 00:28:55 +0900

    Fix audio crackling

--- src/audio/decoder.h
+++ src/audio/decoder.h
@@ -2,6 +2,6 @@
 #include <sndfile.h> // Include the sndfile library for handling audio files
 #include <vector>
 #include "../path.h"
-std::vector<short> decodeAudioToPCM(const path_t &filePath,
-                                    std::vector<short> &buffer,
-                                    SF_INFO &fileInfo);
+bool decodeAudioToPCM(const path_t &filePath,
+                      std::vector<short> &buffer,
+                      SF_INFO &fileInfo);
