2cab8e1cc193e7d570ba9415aa3edefa072da274
VioletXF <zudevxf@gmail.com>
2024-07-21 03:51:30 +0900

    Refactor

---
+++ src/video/VLCInstance.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "vlcpp/Instance.hpp"
+class VLCInstance {
+public:
+  static VLCInstance& getInstance() {
+    static VLCInstance instance;
+    return instance;
+  }
+
+  VLC::Instance* getVLCInstance() {
+    return vlcInstance.get();
+  }
+
+private:
+  VLCInstance() {
+    vlcInstance = std::make_unique<VLC::Instance>(0, nullptr);
+  }
+
+  std::unique_ptr<VLC::Instance> vlcInstance;
+
+  // Delete copy constructor and assignment operator
+  VLCInstance(const VLCInstance&) = delete;
+  VLCInstance& operator=(const VLCInstance&) = delete;
+};
