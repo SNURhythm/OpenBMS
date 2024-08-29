1542cc19cdb84c398444c67a7f17dd29b9181940
VioletXF <zudevxf@gmail.com>
2024-08-30 03:57:07 +0900

    Add math by AI (not verified yet)

--- src/math/Quaternion.cpp
+++ src/math/Quaternion.cpp
@@ -114,3 +114,67 @@ Vector3 Quaternion::getRight() const {
 Vector3 Quaternion::getAxis() const { return {x, y, z}; }
 float Quaternion::getAngle() const { return 2 * acosf(w); }
 Vector3 Quaternion::getAxisAngle() const { float s = sqrtf(1 - w * w); }
+Quaternion Quaternion::operator*(Vector3 other) const {
+  Quaternion q = {other.x, other.y, other.z, 0};
+  Quaternion result = *this * q;
+  return result;
+}
+Quaternion Quaternion::lookRotation(const Vector3 &forward, const Vector3 &up) {
+  Vector3 f = forward.normalized();
+  Vector3 u = up.normalized();
+  Vector3 r = u.cross(f).normalized();
+  u = f.cross(r);
+
+  float m00 = r.x, m01 = r.y, m02 = r.z;
+  float m10 = u.x, m11 = u.y, m12 = u.z;
+  float m20 = f.x, m21 = f.y, m22 = f.z;
+
+  float num8 = (m00 + m11) + m22;
+  Quaternion quaternion;
+  if (num8 > 0.0f) {
+    float num = sqrt(num8 + 1.0f);
+    quaternion.w = num * 0.5f;
+    num = 0.5f / num;
+    quaternion.x = (m12 - m21) * num;
+    quaternion.y = (m20 - m02) * num;
+    quaternion.z = (m01 - m10) * num;
+    return quaternion;
+  }
+  if ((m00 >= m11) && (m00 >= m22)) {
+    float num7 = sqrt(((1.0f + m00) - m11) - m22);
+    float num4 = 0.5f / num7;
+    quaternion.x = 0.5f * num7;
+    quaternion.y = (m01 + m10) * num4;
+    quaternion.z = (m02 + m20) * num4;
+    quaternion.w = (m12 - m21) * num4;
+    return quaternion;
+  }
+  if (m11 > m22) {
+    float num6 = sqrt(((1.0f + m11) - m00) - m22);
+    float num3 = 0.5f / num6;
+    quaternion.x = (m10 + m01) * num3;
+    quaternion.y = 0.5f * num6;
+    quaternion.z = (m21 + m12) * num3;
+    quaternion.w = (m20 - m02) * num3;
+    return quaternion;
+  }
+  float num5 = sqrt(((1.0f + m22) - m00) - m11);
+  float num2 = 0.5f / num5;
+  quaternion.x = (m20 + m02) * num2;
+  quaternion.y = (m21 + m12) * num2;
+  quaternion.z = 0.5f * num5;
+  quaternion.w = (m01 - m10) * num2;
+  return quaternion;
+}
+Quaternion Quaternion::axisAngle(const Vector3 &axis, float angle) {
+  Quaternion quaternion;
+  float halfAngle = angle * 0.5f;
+  float s = sinf(halfAngle);
+
+  quaternion.w = cosf(halfAngle);
+  quaternion.x = axis.x * s;
+  quaternion.y = axis.y * s;
+  quaternion.z = axis.z * s;
+
+  return quaternion;
+}
