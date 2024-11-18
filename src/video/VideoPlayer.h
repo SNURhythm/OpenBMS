eefb66dfd1ec9f7f7763ccd83e6d1b0d93c50883
VioletXF <zudevxf@gmail.com>
2024-11-19 00:31:36 +0900

    Use ffmpeg to render video

--- src/video/VideoPlayer.h
+++ src/video/VideoPlayer.h
@@ -7,17 +7,25 @@
 #include <future>
 #include "../rendering/common.h"
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+}
+
 class VideoPlayer {
 public:
   VideoPlayer();
   ~VideoPlayer();
+
   bool loadVideo(const std::string &videoPath, std::atomic<bool> &isCancelled);
   void update();
   void render();
   void play();
   void pause();
   void stop();
-  void seek(long long micro);
+  void seek(int64_t micro);
   float viewWidth = 1920.0f;
   float viewHeight = 1080.0f;
   int viewId = rendering::bga_view;
@@ -26,19 +34,50 @@ public:
   float fps = 60.0f;
 
 private:
+  std::atomic<bool> isPlaying;
+  std::atomic<bool> isPaused;
+  std::atomic<bool> stopRequested;
+  std::atomic<bool> predecodingActive;
+  std::atomic<int64_t> seekPosition;
+  std::thread predecodeThread;
+
+  AVFormatContext *formatContext = nullptr;
+  AVCodecContext *codecContext = nullptr;
+  AVFrame *frame = nullptr;
+  AVPacket *packet = nullptr;
+  SwsContext *swsContext = nullptr;
+  int videoStreamIndex = -1;
+  std::mutex videoMutex;
+
+  std::queue<AVFrame *> frameBuffer;
+  std::mutex frameBufferMutex;
+  std::condition_variable frameBufferCV;
+  size_t maxBufferSize;
+
+  std::chrono::high_resolution_clock::time_point
+      startTime;             // Start time for playback
+  double lastFramePTS = 0.0; // Last decoded frame's PTS for synchronization
+
+  std::queue<AVFrame *> frameQueue; // Queue for pre-decoded frames
+  std::mutex bufferMutex;           // Protect access to the frame queue
+  std::condition_variable bufferCV; // Condition variable for buffer signaling
+
+  const size_t maxBufferFrames = 30; // Maximum number of frames in the buffer
+  bool isBuffering = false;          // Indicates if buffering is in progress
+
   uint32_t setupFormat(char *chroma, unsigned *width, unsigned *height,
                        unsigned *pitches, unsigned *lines);
-  void *lock(void **planes);
-  void unlock(void *picture, void *const *planes);
-  void display(void *picture);
+  void unloadVideo();
+  void predecodeFrames();
+  void stopPredecoding();
 
   void updateVideoTexture(unsigned int width, unsigned int height);
   std::mutex videoFrameMutex;
 
-  unsigned int videoFrameWidth;
-  unsigned int videoFrameHeight;
+  int videoFrameWidth;
+  int videoFrameHeight;
   int currentFrame = 0;
-  bool videoFrameUpdated;
+  bool hasVideoFrame;
 
   bgfx::UniformHandle s_texY = BGFX_INVALID_HANDLE;
   bgfx::UniformHandle s_texU = BGFX_INVALID_HANDLE;
