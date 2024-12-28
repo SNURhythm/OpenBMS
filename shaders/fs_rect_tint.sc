8960fd2208f620dbd6b474763d291cc5beb9aa5d
VioletXF <zudevxf@gmail.com>
2024-12-28 16:01:00 +0900

    Implement background blur

---
+++ shaders/fs_rect_tint.sc
@@ -0,0 +1,13 @@
+// fs_rect.sc
+$input v_texcoord0
+
+#include "bgfx_shader.sh"
+
+SAMPLER2D(s_texColor, 0);
+uniform vec4 u_tintColor;
+
+void main()
+{
+    vec3 blurred = vec3(texture2D(s_texColor, v_texcoord0).rgb);
+    gl_FragColor = vec4(blurred * u_tintColor.rgb, 1.0);
+}
--- src/rendering/common.h
+++ src/rendering/common.h
@@ -46,11 +46,13 @@ struct PosTexVertex {
 
   static bgfx::VertexLayout ms_decl;
 };
-
-static bgfx::ViewId ui_view = 255;
+static bgfx::ViewId final_view = 5;
+static bgfx::ViewId ui_view = 254;
 static bgfx::ViewId bga_view = 1;
 static bgfx::ViewId bga_layer_view = 2;
 static bgfx::ViewId main_view = 128;
+static bgfx::ViewId blur_view_h = 3;
+static bgfx::ViewId blur_view_v = 4;
 static bgfx::ViewId clear_view = 0;
 extern Camera *main_camera;
 extern Camera game_camera;
@@ -72,4 +74,51 @@ static PosTexCoord0Vertex s_quadVertices[] = {
     {1.0f, -1.0f, 0.0f, 1.0f, 1.0f},
 };
 static const uint16_t s_quadIndices[] = {0, 1, 2, 1, 3, 2};
+inline void screenSpaceQuad()
+{
+  bgfx::TransientVertexBuffer tvb{};
+  bgfx::TransientIndexBuffer tib{};
+
+  //  SDL_Log("Rendering video texture frame %d; time: %f", currentFrame,
+  //  currentFrame / 30.0f);
+
+  bgfx::VertexLayout &layout = rendering::PosTexCoord0Vertex::ms_decl;
+  bgfx::allocTransientVertexBuffer(&tvb, 4, layout);
+  bgfx::allocTransientIndexBuffer(&tib, 6);
+  auto *vertex = (rendering::PosTexCoord0Vertex *)tvb.data;
+
+  // Define quad vertices
+  vertex[0].x = -1.0f;
+  vertex[0].y = -1.0f;
+  vertex[0].z = 0.0f;
+  vertex[0].u = 0.0f;
+  vertex[0].v = 1.0f;
+
+  vertex[1].x = 1.0f;
+  vertex[1].y = -1.0f;
+  vertex[1].z = 0.0f;
+  vertex[1].u = 1.0f;
+  vertex[1].v = 1.0f;
+
+  vertex[2].x = -1.0f;
+  vertex[2].y = 1.0f;
+  vertex[2].z = 0.0f;
+  vertex[2].u = 0.0f;
+  vertex[2].v = 0.0f;
+
+  vertex[3].x = 1.0f;
+  vertex[3].y = 1.0f;
+  vertex[3].z = 0.0f;
+  vertex[3].u = 1.0f;
+  vertex[3].v = 0.0f;
+  auto *indices = (uint16_t *)tib.data;
+  indices[0] = 0;
+  indices[1] = 1;
+  indices[2] = 2;
+  indices[3] = 1;
+  indices[4] = 3;
+  indices[5] = 2;
+  bgfx::setVertexBuffer(0, &tvb);
+  bgfx::setIndexBuffer(&tib);
+}
 } // namespace rendering
