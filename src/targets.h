843ccfd6246f7557a7190a5a9f22e58c2ff28219
VioletXF <zudevxf@gmail.com>
2025-07-23 01:05:42 +0900

    Fix build

--- src/targets.h
+++ src/targets.h
@@ -1,64 +1,39 @@
+#pragma once
 #ifdef _WIN32
 #include <windows.h>
-#define TARGET_OS_WINDOWS 1
-constexpr bool TARGET_IS_WINDOWS = true;
-constexpr bool TARGET_IS_APPLE = false;
-constexpr bool TARGET_IS_LINUX = false;
-constexpr bool TARGET_IS_OSX = false;
-constexpr bool TARGET_IS_IOS = false;
-constexpr bool TARGET_IS_IOS_SIMULATOR = false;
 
 #elif __APPLE__
 
 #include "TargetConditionals.h"
 #if TARGET_OS_IPHONE && TARGET_IPHONE_SIMULATOR
 // define something for simulator
-#define TARGET_OS_IOS_SIMULATOR 1
-constexpr bool TARGET_IS_WINDOWS = false;
-constexpr bool TARGET_IS_APPLE = true;
-constexpr bool TARGET_IS_LINUX = false;
-constexpr bool TARGET_IS_OSX = false;
-constexpr bool TARGET_IS_IOS = true;
-constexpr bool TARGET_IS_IOS_SIMULATOR = true;
 #elif TARGET_OS_IPHONE
-#define TARGET_OS_IOS 1
-constexpr bool TARGET_IS_WINDOWS = false;
-constexpr bool TARGET_IS_APPLE = true;
-constexpr bool TARGET_IS_LINUX = false;
-constexpr bool TARGET_IS_OSX = false;
-constexpr bool TARGET_IS_IOS = true;
-constexpr bool TARGET_IS_IOS_SIMULATOR = true;
 #else
 #define TARGET_OS_OSX 1
-constexpr bool TARGET_IS_WINDOWS = false;
-constexpr bool TARGET_IS_APPLE = true;
-constexpr bool TARGET_IS_LINUX = false;
-constexpr bool TARGET_IS_OSX = true;
-constexpr bool TARGET_IS_IOS = false;
-constexpr bool TARGET_IS_IOS_SIMULATOR = false;
 #endif
 #elif __linux
 #define TARGET_OS_LINUX 1
-constexpr bool TARGET_IS_WINDOWS = false;
-constexpr bool TARGET_IS_APPLE = false;
-constexpr bool TARGET_IS_LINUX = true;
-constexpr bool TARGET_IS_OSX = false;
-constexpr bool TARGET_IS_IOS = false;
-constexpr bool TARGET_IS_IOS_SIMULATOR = false;
 #elif __unix // all unices not caught above
 // Unix
-constexpr bool TARGET_IS_WINDOWS = false;
-constexpr bool TARGET_IS_APPLE = false;
-constexpr bool TARGET_IS_LINUX = false;
-constexpr bool TARGET_IS_OSX = false;
-constexpr bool TARGET_IS_IOS = false;
-constexpr bool TARGET_IS_IOS_SIMULATOR = false;
 #elif __posix
 // POSIX
-constexpr bool TARGET_IS_WINDOWS = false;
-constexpr bool TARGET_IS_APPLE = false;
-constexpr bool TARGET_IS_LINUX = false;
-constexpr bool TARGET_IS_OSX = false;
-constexpr bool TARGET_IS_IOS = false;
-constexpr bool TARGET_IS_IOS_SIMULATOR = false;
+#endif
+
+enum TargetPlatform {
+  Windows,
+  MacOS,
+  Linux,
+  iOS
+};
+
+#if TARGET_OS_OSX
+constexpr TargetPlatform TARGET_PLATFORM = MacOS;
+#elif TARGET_OS_IOS
+constexpr TargetPlatform TARGET_PLATFORM = iOS;
+#elif TARGET_OS_IPHONE
+constexpr TargetPlatform TARGET_PLATFORM = iOS;
+#elif TARGET_OS_LINUX
+constexpr TargetPlatform TARGET_PLATFORM = Linux;
+#else
+constexpr TargetPlatform TARGET_PLATFORM = Windows;
 #endif
