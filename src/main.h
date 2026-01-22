7b27ec37f812bf818c9dc57dd4aae71f202009b0
VioletXF <zudevxf@gmail.com>
2026-01-22 17:31:09 +0900

    Refactor blur

--- src/main.h
+++ src/main.h
@@ -3,4 +3,6 @@
 #include <cstdint>
 int main(int argv, char **args);
 void run();
-void resetViewTransform(uint16_t bgaWidth, uint16_t bgaHeight);
+void resetViewTransform(uint16_t bgaWidth, uint16_t bgaHeight,
+                        bgfx::ViewId blurViewH, bgfx::ViewId blurViewV,
+                        bgfx::ViewId finalView);
